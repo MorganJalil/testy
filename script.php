54ea0cdd46e941e16defadecc05c983383f9a330
Morgan J <Moggelicious@hotmail.com>
2018-10-29 13:53:02 +0100

    test2

--- script.php
+++ script.php
@@ -1,6 +1,9 @@
 <?php
-function addNumbers($a, $b) {
+function addNumbers($numbers) {
     $sum = $a + $b;
+    foreach($numbers as $number) {
+      $sum += $number;
+    }
     return $sum;
   }
 ?>
